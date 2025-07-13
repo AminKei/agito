cedd9baa96e7ca6748a549234f3ffaeb0c3423fa
AminKei <moakeimasi@gmail.com>
2025-07-13 18:52:35 +0330

    routhi

--- src/Config/Routes/RoutesConfig.tsx
+++ src/Config/Routes/RoutesConfig.tsx
@@ -2,7 +2,6 @@ import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import ScreenLoading from "../../Components/ScreenLoading/ScreenLoading";
 import Error404 from "../../Components/404/404";
-import { RoutePath } from "../../Constants/Routes.Path";
 
 const Home = lazy(() => import("../../Pages/Home/Home"));
 const AddAd = lazy(() => import("../../Pages/AddAd/AddAd"));
@@ -20,12 +19,15 @@ const RoutesConfig: React.FC<RoutesConfigProps> = ({ user, setUser }) => {
   return (
     <Suspense fallback={<ScreenLoading />}>
       <Routes>
-        <Route path={RoutePath.basePath} element={<Home />} />
-        <Route path={RoutePath.add} element={<AddAd />} />
-        <Route path={RoutePath.rulespage} element={<RulesPage user={user} />} />
-        <Route path={RoutePath.auth.signup} element={<SignUp setUser={setUser} />} />
-        <Route path={RoutePath.adspage} element={<AdsPage />} />
-        <Route path={RoutePath.profile} element={<Profile setUser={setUser} user={user} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddAd />} />
+        <Route path="/rulespage" element={<RulesPage user={user} />} />
+        <Route path="/signup" element={<SignUp setUser={setUser} />} />
+        <Route path="/adspage/:id" element={<AdsPage />} />
+        <Route
+          path="/profile"
+          element={<Profile setUser={setUser} user={user} />}
+        />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </Suspense>
